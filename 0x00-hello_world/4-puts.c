2f61cf95e191a39447d48bcacac48ba76fe36dab
Mcherut-o <mchepkoecht@gmail.com>
2023-06-19 11:41:56 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that uses the formatted output of a puts function
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
