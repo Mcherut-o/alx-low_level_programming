c8dedc7f6e5b5a3b2ab2e77d28c1fc18cfa13cfc
Mcherut-o <mchepkoecht@gmail.com>
2023-06-19 18:21:10 -0700

    Alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - A program that prints the alphabets in lowwercase
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c;
+n = 'a';
+while (c <= 'z')
+{
+putchar("c");
+c++;
+}
+putchar("\n");
+return (0);
+}
