c6416002038117ec5904510c155a75403df16b7b
Mcherut-o <mchepkoecht@gmail.com>
2023-06-25 11:34:24 -0700

    Multiplication

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *times_table - Afunction that does multiplication
+ *Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+int i, j, times;
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+times = i * j;
+if (times <= 9)
+{
+_putchar(times + '0');
+}
+else
+{
+_putchar((times / 10) + '0');
+_putchar((times % 10) + '0');
+}
+if (j < 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
+}
