f138de0546fb4ca8a64d3e84a2aa7a196868938f
Mcherut-o <mchepkoecht@gmail.com>
2023-06-25 16:29:11 -0700

    Addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,10 @@
+/**
+ *add - A function that adds two integers
+ *@num1: stores values
+ *@num2: stores value
+ * Return: Always 0 (Success)
+ */
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
