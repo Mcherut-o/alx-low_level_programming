6ec401514211168924b4ffc6545eba052c619bca
Mcherut-o <mchepkoecht@gmail.com>
2023-06-25 07:38:08 -0700

    Absolute value of an integer is printed finally

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - A function that computes the absolute value of an integer
+ *Return: Always 0 (Success)
+ */
+
+int _abs(int b)
+{
+if (b < 0)
+{
+b = -b;
+}
+return (b);
+}
