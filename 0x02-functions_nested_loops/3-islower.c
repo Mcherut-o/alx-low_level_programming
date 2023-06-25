f49b9eda9f4fc012cf6fbd358af1f88ae7c436a2
Mcherut-o <mchepkoecht@gmail.com>
2023-06-25 05:14:55 -0700

    Been checking for lower characters for a while mow

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - Prints output with lowercase checked
+*int _islower(int c) - A function that searches for lowercase charaters
+*int _putchar(int c) - A function that prints output
+*@c - Stores a value
+*@r - Stores value
+* Return: 1 if c is lowercase and 0 otherwise
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else
+return (0);
+}
+int _putchar(char c)
+{
+return write(1, &c, 1);
+}
