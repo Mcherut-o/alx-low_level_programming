9db2c60a8c1cdeae10c60ba5b4aef13998ecc255
Mcherut-o <mchepkoecht@gmail.com>
2023-07-06 15:45:26 -0700

    Length of a string returned

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,14 @@
 
 /**
 *_strlen - A function that returns the length of a string
-*@s: Stores a value 
+*@s: Stores a value
 *Return: Always 0 (Success)
 */
 
 int _strlen(char *s)
 {
 int lengths = 0;
-while(*s != '\0')
+while (*s != '\0')
 {
 lengths++;
 s++;
