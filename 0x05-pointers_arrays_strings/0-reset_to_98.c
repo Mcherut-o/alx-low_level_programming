7c20e4820450d41302b204c60602807be85c0941
Mcherut-o <mchepkoecht@gmail.com>
2023-07-06 15:25:18 -0700

    Finally made the pointer execute

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset _to_98 - A function that updates the value that it points to
+ *@n: Stores the variable of the value
+ *Return : Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *swap_int - A function  to swap values
+ *@a:Stores values
+ *@b:Stores values
+ *Return: Always 0 (Success)
+ */
+
+void swap_int(int *a, int *b)
+{
+int n;
+n = *a;
+*a = *b;
+*b = n;
+}
