2ee8e8f1c61e716890ffe494225db8a6192753c1
Mcherut-o <mchepkoecht@gmail.com>
2023-07-06 15:36:04 -0700

    Just swapped two numbers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *swap_int - A function  to swap values
- *@a:Stores values
- *@b:Stores values
- *Return: Always 0 (Success)
- */
+*swap_int - A function  to swap values
+*@a:Stores values
+*@b:Stores values
+*Return: Always 0 (Success)
+*/
 
 void swap_int(int *a, int *b)
 {
