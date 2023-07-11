01b9189b063fb9ce0d22e73eae4bbbb855fb9e57
Mcherut-o <mchepkoecht@gmail.com>
2023-07-11 10:47:30 -0700

    Pointer created to a new string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,10 +1,25 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * create_array - create array of size size and assign char c
- * @size: size of array
- * @c: char to assign
- * Description: create array of size size and assign char c
- * Return: pointer to array, NULL if fail
- *
- */
+* _strdup - A function that returns a pointer to a new string
+* @str: A character
+* Return: A pointer to string
+*       : NULL if str = NULL
+*/
+
+char *_strdup(char *str)
+{
+char *m;
+int i, j = 0;
+if (str == NULL)
+return (NULL);
+i = 0;
+while (str[i] != '\0')
+i++;
+m = malloc(sizeof(char) * (i + 1));
+if (m == NULL)
+return (NULL);
+for (j = 0; str[j]; j++)
+m[j] = str[j];
+return (m);
+}
