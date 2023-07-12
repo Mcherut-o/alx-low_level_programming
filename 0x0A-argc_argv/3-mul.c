6a7b62d66806f8e3ccc0ada4a35c37a34d46eec1
Mcherut-o <mchepkoecht@gmail.com>
2023-07-12 06:18:23 -0700

    2 numbers multiplied

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,60 +2,55 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * _atoi - A function that converts a string to an integer
+ * @s: String
+ * Return: Always 0 (Success)
  */
+
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int a, b, c, strlength, e, f;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+	a = b = c = strlength = e = f = 0;
 
-	while (s[len] != '\0')
-		len++;
+	while (s[strlength] != '\0')
+		strlength++;
 
-	while (i < len && f == 0)
+	while (a < strlength && e == 0)
 	{
-		if (s[i] == '-')
-			++d;
+		if (s[a] == '-')
+			++b;
 
-		if (s[i] >= '0' && s[i] <= '9')
+		if (s[a] >= '0' && s[a] <= '9')
 		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
+			f = s[a] - '0';
+			if (b % 2)
+				f = -f;
+			c = c * 10 + f;
+			e = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '9')
 				break;
-			f = 0;
+			e = 0;
 		}
-		i++;
+		a++;
 	}
 
-	if (f == 0)
+	if (e == 0)
 		return (0);
 
-	return (n);
+	return (c);
 }
 
 /**
- * main - multiplies two numbers
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success), 1 (Error)
+ * main - This function prints minimum number of coins to make change
+ * @argc: Number of command line arguments
+ * @argv: Array containing program command line arguments
+ * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[])
 {
-	int result, num1, num2;
+	int total, dgt1, dgt2;
 
 	if (argc < 3 || argc > 3)
 	{
@@ -63,11 +58,11 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	num1 = _atoi(argv[1]);
-	num2 = _atoi(argv[2]);
-	result = num1 * num2;
+	dgt1 = _atoi(argv[1]);
+	dgt2 = _atoi(argv[2]);
+	total = dgt1 * dgt2;
 
-	printf("%d\n", result);
+	printf("%d\n", total);
 
 	return (0);
 }
