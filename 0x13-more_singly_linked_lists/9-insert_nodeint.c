44272e1a63a4ff50ebe92748addaf6099d1dc2c1
Mcherut-o <mcherutok@gmail.com>
2023-08-01 06:43:33 -0700

    Updated more singly linked lists (DSA

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,11 +15,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new_node, *current_node;
 	unsigned int ct = 0;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-
 	new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
